21e9508af52ef2c135af4700f3140fa343429303
Brian Ying <humanbying@gmail.com>
2016-08-15 10:52:36 -0700

    why can't i  get this to work...

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const TableStuff = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>I'm the Table Page!</h1>
+      </div>
+
+    )
+  }
+})
+
+ReactDOM.render(
+  <TableStuff />,
+  document.getElementById("example")
+);
