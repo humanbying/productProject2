798458a653e67cbd26a047a702ea31cdb808f5bc
Brian Ying <humanbying@gmail.com>
2016-08-15 11:19:06 -0700

    next step is to map out list

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 
 const TableStuff = React.createClass({
+  getInitialState: function(){
+    return{
+      name: '',
+      description: '',
+      price: ''
+    }
+  },
   render: function() {
     return (
       <div>
-        <h1>I'm the Table Page!</h1>
+      <form onSubmit={this.onSubmit}>
+        <h1>Name</h1>
+        <input id = "name" type="text" ref="text"/>
+        <h1>Description</h1>
+        <input id = "description" type="text" ref="text"/>
+        <h1>Price</h1>
+        <input id = "price" type="text" ref="text"/>
+        <button>Submit</button>
+        </form>
       </div>
-
     )
+  },
+  onSubmit: function(e){
+    e.preventDefault();
+    var text = this.refs.text.value;
+
+    if(!text){
+      alert('enter an item');
+      return;
+    }
   }
-})
+});
 
 ReactDOM.render(
   <TableStuff />,
-  document.getElementById("example")
+  document.getElementById('example')
 );
